96527314cbbe5db5c089a67fee4ac55e8b752ddc
enes-arisoy <enes.arisoy.85@gmail.com>
2025-04-20 03:45:35 +0300

    project page added

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-const menuBtn =document.querySelector("#menu-btn");
+const menuBtn = document.querySelector("#menu-btn");
 const openNav = document.querySelector("nav");
 
 menuBtn.addEventListener("click", () => {
-    openNav.classList.toggle("open-nav");
+  openNav.classList.toggle("open-nav");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const videos = document.querySelectorAll("video");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.play().catch((error) => {
+            console.error("Video oynatılamadı:", error);
+          });
+          if (!entry.target.dataset.endedListener) {
+            entry.target.addEventListener("ended", () => {
+              entry.target.currentTime = 0;
+              entry.target.play();
+            });
+            entry.target.dataset.endedListener = "true";
+          }
+        } else {
+          entry.target.pause();
+        }
+      });
+    },
+    { threshold: 1 }
+  );
+  videos.forEach((video) => {
+    observer.observe(video);
+  });
 });
